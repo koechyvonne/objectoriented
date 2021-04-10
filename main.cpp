35a089a3e56d0f68b12b16429b1dc79da1a9ed46
koechyvonne <yserene842@gmail.com>
2021-04-10 10:38:22 +0000

    my cpp program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+8 #include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string name = "Name"; 
+cout << "Type your name: ";
+// cin >> name;
+cout << "Hello World!" << name;
+return 0;
+}
+ 
+
+
+
+
